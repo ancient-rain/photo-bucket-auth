ee9df42795bcef80b0036b090350bd03086a171f
askalerion <bednartd@rose-hulman.edu>
2017-07-23 14:32:02 -0400

    Added installation packages

---
+++ PhotoBucketAuth/src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBd6Mlb2c16kOGxM7N3JDXiMXilBWlMu_g",
+    authDomain: "bednartd-photo-bucket-1255d.firebaseapp.com",
+    databaseURL: "https://bednartd-photo-bucket-1255d.firebaseio.com",
+    projectId: "bednartd-photo-bucket-1255d",
+    storageBucket: "bednartd-photo-bucket-1255d.appspot.com",
+    messagingSenderId: "449603636387"
+  },
+};
---
+++ PhotoBucketAuth/src/environments/environment.ts
@@ -0,0 +1,16 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBd6Mlb2c16kOGxM7N3JDXiMXilBWlMu_g",
+    authDomain: "bednartd-photo-bucket-1255d.firebaseapp.com",
+    databaseURL: "https://bednartd-photo-bucket-1255d.firebaseio.com",
+    projectId: "bednartd-photo-bucket-1255d",
+    storageBucket: "bednartd-photo-bucket-1255d.appspot.com",
+    messagingSenderId: "449603636387"
+  },
+};
